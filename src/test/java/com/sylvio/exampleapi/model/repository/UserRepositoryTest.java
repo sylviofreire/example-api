1535c464114fd5243f6227cae5a83eba9e6cb434
Sylvio Freire <sylviosfreire@hotmail.com>
2020-11-13 23:50:04 -0300

    Implements find by id integration test

--- src/test/java/com/sylvio/exampleapi/model/repository/UserRepositoryTest.java
+++ src/test/java/com/sylvio/exampleapi/model/repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.sylvio.exampleapi.model.repository;
 
+import com.sylvio.exampleapi.model.entity.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -19,7 +25,19 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository repository;
 
-    //@Test
+    @Test
+    @DisplayName("Should get user by id")
+    public void findByIdTest(){
+        //Scenario
+        User user = User.builder().name("Sylvio").gender("Masculino").birthDate("25/08/1994").age(32).build();
+        entityManager.persist(user);
 
+        //Execution
+        Optional<User> foundUser =  repository.findById(user.getId());
+
+        //Verifications
+        assertThat(foundUser.isPresent()).isTrue();
+
+    }
 
 }
