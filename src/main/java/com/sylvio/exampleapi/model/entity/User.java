51baacf0a6e3af4ecd18a0906b718ba43a98ad13
Sylvio Freire <sylviosfreire@hotmail.com>
2020-11-13 23:49:34 -0300

    Mapping User entity

--- src/main/java/com/sylvio/exampleapi/model/entity/User.java
+++ src/main/java/com/sylvio/exampleapi/model/entity/User.java
@@ -5,12 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String gender;
     private String birthDate;
